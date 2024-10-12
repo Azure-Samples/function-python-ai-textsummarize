03116b8470f2a306c52a3afbc69ef4734bfde6f8
Paul Yuknewicz <paulyuk@microsoft.com>
2024-10-12 01:14:23 -0700

    Function code now uses Entra Identity
    Signed-off-by: Paul Yuknewicz <paulyuk@microsoft.com>

--- text_summarize/function_app.py
+++ text_summarize/function_app.py
@@ -1,27 +1,37 @@
 import azure.functions as func
 import logging
 from azure.ai.textanalytics import TextAnalyticsClient
-from azure.core.credentials import AzureKeyCredential
+from azure.identity import DefaultAzureCredential
 import os
 
 app = func.FunctionApp()
 
 # Load AI url and secrets from Env Variables in Terminal before running, 
-# e.g. `export AI_URL=https://***.cognitiveservices.azure.com/`
-key = os.getenv('AI_SECRET', 'SETENVVAR!') 
-endpoint = os.getenv('AI_URL', 'SETENVVAR!') 
+# e.g. `export TEXT_ANALYTICS_ENDPOINT=https://<your subdomain>.cognitiveservices.azure.com/`
+endpoint = os.getenv('TEXT_ANALYTICS_ENDPOINT', 'SETENVVAR!') 
 
-# Authenticate the client using your key and endpoint 
+# Create client using Entra User or Managed Identity (no longer AzureKeyCredential)
+# This requires a sub domain name to be set in endpoint URL for Managed Identity support
+# See https://learn.microsoft.com/en-us/azure/ai-services/authentication#authenticate-with-microsoft-entra-id 
 text_analytics_client = TextAnalyticsClient(
     endpoint=endpoint,
-    credential=AzureKeyCredential(key),
+    credential=DefaultAzureCredential(),
 )
+
+@app.function_name(name="summarize_function")
+@app.blob_trigger(arg_name="myblob", path="unprocessed-text/{name}",
+                  connection="blobstorage")
+@app.blob_output(arg_name="outputblob", path="processed-text/{name}-output.txt", connection="blobstorage")
+def test_function(myblob: func.InputStream, outputblob: func.Out[str]):
+   logging.info(f"Triggered item: {myblob.name}\n")
+
+   document = [myblob.read().decode('utf-8')]
+   summarized_text = ai_summarize_txt(document)
+   logging.info(f"\n *****Summary***** \n{summarized_text}");
+   outputblob.set(summarized_text)
 
 # Example method for summarizing text
 def ai_summarize_txt(document):
-    import os
-    from azure.core.credentials import AzureKeyCredential
-    from azure.ai.textanalytics import TextAnalyticsClient
 
     poller = text_analytics_client.begin_extract_summary(document)
     extract_summary_results = poller.result()
@@ -39,16 +49,3 @@ def ai_summarize_txt(document):
                 result.error.code, result.error.message
             ))
             logging.error(f"Error with code '{result.error.code}' and message '{result.error.message}'")
-
-
-@app.function_name(name="summarize_function")
-@app.blob_trigger(arg_name="myblob", path="test-samples-trigger/{name}",
-                  connection="blobstorage")
-@app.blob_output(arg_name="outputblob", path="test-samples-output/{name}-output.txt", connection="blobstorage")
-def test_function(myblob: func.InputStream, outputblob: func.Out[str]):
-   logging.info(f"Triggered item: {myblob.name}\n")
-
-   document = [myblob.read().decode('utf-8')]
-   summarized_text = ai_summarize_txt(document)
-   logging.info(f"\n *****Summary***** \n{summarized_text}");
-   outputblob.set(summarized_text)
